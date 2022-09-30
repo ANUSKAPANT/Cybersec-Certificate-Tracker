3d079051f0f97a4da054f7b6c8eef86194507872
Anuska Pant <anuska.pant@gmail.com>
2022-09-30 03:04:38 -0500

    skip unnecessary token verification and remove unwanted files

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :verify_authenticity_token, :only => :create
 
   def destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
