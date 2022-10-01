e774c622766d3ca2c7d8db886b22b69d16509a90
Yatin Gupta <yatingupta7777@gmail.com>
2022-10-01 18:03:00 -0500

    add test for sign in path

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,9 +1,14 @@
 sign_in_text = "You need to sign in or sign up before continuing."
+sign_in_path = "/users/sign_in"
 
 Given("user is not signed in") do
-visit "/"
+    visit "/"
 end
 
-Then("I should be told You need to sign in or sign up before continuing.") do
-expect(page.find('.alert').text).to eq(sign_in_text)
+Then("user should be told You need to sign in or sign up before continuing.") do
+    expect(page.find('.alert').text).to eq(sign_in_text)
+end
+
+Then("user should be redirected to sign in page") do 
+    expect(page.current_path).to eq(sign_in_path)
 end
--- features/user_login.feature
+++ features/user_login.feature
@@ -3,4 +3,8 @@ Feature: User is asked to sign in
 
   Scenario: User opens the webpage "/"
     Given user is not signed in
-    Then I should be told You need to sign in or sign up before continuing.
+    Then user should be told You need to sign in or sign up before continuing.
+
+  Scenario: User opens the webpage "/"
+    Given user is not signed in
+    Then user should be redirected to sign in page
