a119b324e1c68e9f03ad55525b4d31064c3ba9b2
mesbaulalam <mesbaul.a.khan@gmail.com>
2022-10-03 19:14:59 -0500

    Implemented Logout and changes to Login Page UI

--- app/javascript/cybersec_cert_tracker/Dashboard.jsx
+++ app/javascript/cybersec_cert_tracker/Dashboard.jsx
@@ -1,32 +1,13 @@
 import React, { useEffect } from "react";
 import { Button } from "reactstrap";
-import DashboardTable from "./DashboardTable";
 import "./table.css";
 import { useStoreContext } from "./Store";
 import { setUserData } from "./Actions";
-import axios from "axios";
-import makeData from "./makeData";
 
 function Dashboard() {
   const { globalState, dispatch } = useStoreContext();
-  const data = React.useMemo(() => makeData(100000), []);
 
-  return (
-    <div className="table-container">
-      <h1>Hello {globalState.userData.role}!</h1>
-      {globalState.userData.role == "admin" && (
-        <div>
-          <Button color="primary" size="lg" className="btn-1 ">
-            + Import CSV
-          </Button>{" "}
-          <Button color="success" size="lg" className="btn-2 mx-2">
-            Add Data
-          </Button>
-        </div>
-      )}
-      <DashboardTable data={data} />;
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
 
 export default Dashboard;
