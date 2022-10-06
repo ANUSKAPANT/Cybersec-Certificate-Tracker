7c26d60960b4c950447ffdfd1ea28a4d7f6b48da
aayush97 <agautam1997@gmail.com>
2022-10-06 00:20:13 -0500

    Add has_many and has_one associations

--- app/models/cert_voucher.rb
+++ app/models/cert_voucher.rb
@@ -1,3 +1,4 @@
 class CertVoucher < ApplicationRecord
   belongs_to :student_course
+  has_one :exam
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   belongs_to :company
+  has_many :student_courses
 end
