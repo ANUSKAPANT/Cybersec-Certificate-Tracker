94f4f1b327e706d126556e4953fb454dadfbfda8
Yatin Gupta <yatingupta7777@gmail.com>
2022-10-08 10:56:04 -0500

    add reactstrap form

--- app/javascript/cybersec_cert_tracker/Dashboard.jsx
+++ app/javascript/cybersec_cert_tracker/Dashboard.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./table.css";
 import DashboardTable from "./DashboardTable";
 import makeData from "./makeData";
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 function Dashboard() {
   const data = React.useMemo(() => makeData(100000), []);
 
-  return <DashboardTable data={data} />;
+  return <>
+  <Label for="exampleCustomFileBrowser">Upload CSV</Label>
+  <Input type="file" id="exampleCustomFileBrowser" name="customFile" accept=".csv" multiple/>
+  <DashboardTable data={data} />
+  </>;
 }
 
 export default Dashboard;
