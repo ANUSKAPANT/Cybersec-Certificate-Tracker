09f1b26db122e917a2246817719a8d02575eddc1
Anuska Pant <anuska.pant@gmail.com>
2022-10-18 07:56:37 -0500

    comprehensive docker setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+usr = User.create(email: 'demor@example.com', password: 'test123', password_confirmation: 'test123')
+puts usr.attributes
