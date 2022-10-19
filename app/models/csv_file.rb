ee75ad875c85d5826b705257e17c3de82d0196eb
aayush97 <agautam1997@gmail.com>
2022-10-18 19:40:23 -0500

    Add student records from csv

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -19,6 +19,7 @@ class CsvFile < ApplicationRecord
       "STUDENT NAME" => "full_name_canvas",
       "CREATED AT" => "course_registration_date",
       "LISTING NAME"=> "course_name",
+      "ID" => "canvas_id"
     }
     header_map["#{header}"]
   end
@@ -27,16 +28,50 @@ class CsvFile < ApplicationRecord
     Company.find_by(name: row['company_name']) || Company.create(name: row['company_name'], smc: row['smc'] || false)
   end
 
+  def create_or_update_student_record(row, company)
+    student = Student.find_by(email_id: row['email_id'])
+    if row['full_name_tees']
+      first_name = row['full_name_tees'].split(',')[1]
+      last_name = row['full_name_tees'].split(',')[0]
+    elsif row ['full_name_canvas']
+      first_name = row['full_name_canvas'].split(' ')[0]
+      last_name = row['full_name_canvas'].split(' ')[-1]
+    end
+    if student
+      # don't replace company name by unknown if already present
+      # can happen when canvas is uploaded after TEES edge
+      if company.name == 'Unknown'
+        company.id = student.company_id
+      end
+      # don't replace title by null if already present
+      # can happen if canvas is uploaded after tees edge
+      if not row['title']
+        row['title'] = student.title
+      end
+      # don't replace title by null if already present
+      # can happen if tees edge is uploaded after canvas
+      if not row['canvas_id']
+        row['canvas_id'] = student.canvas_id
+      end
+      student.update(email_id: row['email_id'], first_name: first_name, last_name: last_name, title:row['title'], canvas_id: row['canvas_id'], company_id: company.id)
+    else
+      student = Student.create(email_id: row['email_id'], first_name: first_name, last_name: last_name, title:row['title'], canvas_id: row['canvas_id'], company_id: company.id)
+    end
+    student
+  end
+
   def create_records(file_path)
     converter = lambda { |header| map_headers(header.upcase) }
     file = File.open(file_path, "r:bom|utf-8")
     csv = CSV.parse(file, :headers => true, header_converters: converter)
     csv.each do |row|
-    # add company
+      # add company
       if not row['company_name'] || row['company_name']==''
         row['company_name'] = 'Unknown'
       end
       company = add_company_record(row)  
+      # add student
+      student = create_or_update_student_record(row, company) 
     end
   end
 end
