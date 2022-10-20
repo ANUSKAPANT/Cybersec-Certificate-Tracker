b5ced19fc8537769ccdbf9368dd55035ff58c65e
aayush97 <agautam1997@gmail.com>
2022-10-20 08:31:51 -0500

    Add API endpoint for Company table

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :records, only: :index
   resources :vendors
   resources :students
+  resources :companies
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
