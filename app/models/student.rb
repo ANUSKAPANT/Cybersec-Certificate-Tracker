75a72c0dbdf869d149f4104b552852bd57fcf0ef
Anuska Pant <anuska.pant@gmail.com>
2022-10-24 03:34:43 -0500

    student relationship with course added through student courses table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :company
   has_many :student_courses
+  has_many :courses, through: :student_courses
   validates :first_name, presence: true
   validates :last_name, presence: true
 
