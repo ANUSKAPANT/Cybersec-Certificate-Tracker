5856fdde45624b2c3f586485279c885654db2473
Sabina Adhikari <sabinaadhikaris@Sabinas-MacBook-Pro.local>
2022-10-25 14:15:05 -0500

    added modal

--- app/javascript/cybersec_cert_tracker/Dashboard.jsx
+++ app/javascript/cybersec_cert_tracker/Dashboard.jsx
@@ -2,14 +2,39 @@ import React, { useEffect, useState } from "react";
 import "./table.css";
 import DashboardTable from "./DashboardTable";
 import makeData from "./makeData";
-import { Label, Input } from 'reactstrap';
+import { Label, Input, Button} from 'reactstrap';
+import Modal from 'react-modal'
+// import Moda from "./Moda";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+function submit_form() {
+  document.formu1.reset(); 
+  }
+
 function Dashboard({userData}) {
   const [tableData, setTableData] = useState([]);
+  const handleShow = () => setShow(true);
+  // const[isOpen, setIsOpen] = useState(false)
+  const[modelIsOpen, setModalIsOpen] = useState(false)
 
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+  }
+
+  const BUTTON_WRAPPER_STYLES = {
+    position: 'relative',
+    zIndex: 1
+  }
+
+  const OTHER_CONTENT_STYLES = {
+    position: 'relative',
+    zIndex: 2,
+    backgroundColor: 'red',
+    padding: '10px'
+  }
+  
   useEffect(() => {
     axios.get('/records.json', { headers: {"Authorization" : `Bearer ${userData.token}`}})
       .then((res) => {
@@ -67,8 +92,63 @@ function Dashboard({userData}) {
   <ToastContainer/>
   <Label for="exampleCustomFileBrowser">Upload CSV</Label>
   <Input type="file" id="exampleCustomFileBrowser" name="csvFile" accept=".csv" multiple onChange={fileUpload}/>
+
+  <div>
+    <button onClick={() => setModalIsOpen(true)}> 
+      Add Student Information
+    </button>
+    <Modal 
+      isOpen = {modelIsOpen} 
+      shouldCloseOnOverlayClick = {false}
+      onRequestClose={() => setModalIsOpen(false)}>
+      <h2>
+        Student Details
+      </h2>
+      <form class = "form-inline">
+        <div class="form-row align-items-center">
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputName">Name</label>
+            First Name: <input type="text" class="form-control" id="inlineFormInputName" placeholder="First Name"></input>
+          </div>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Last Name"></input>
+            </div>
+          </div>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputName">Name</label>
+            <input type="text" class="form-control" id="inlineFormInputName" placeholder="Certificate Name"></input>
+          </div>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Email Address"></input>
+            </div>
+          </div>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputName">Name</label>
+            <input type="text" class="form-control" id="inlineFormInputName" placeholder="Company Name"></input>
+          </div>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Registration Date"></input>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="cancel" onClick = {() => setModalIsOpen(false)} class="btn btn-primary mb-2" data-dismiss="modal">Cancel</button> 
+            {/* <button type="cancel" onClick = "submit_form()" >Cancel</button>  */}
+            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  </div>
+
   <DashboardTable data={tableData} />
   </>;
 }
 
-export default Dashboard;
+export default Dashboard; 
+
