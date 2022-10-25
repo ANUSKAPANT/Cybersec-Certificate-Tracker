53ad82c1c2d0ec3ca9e27aef401ec88fbb0d4851
mesbaulalam <mesbaul.a.khan@gmail.com>
2022-10-24 21:33:44 -0500

    Fixed number of records bug and added sorting functionality

--- app/javascript/cybersec_cert_tracker/Dashboard.jsx
+++ app/javascript/cybersec_cert_tracker/Dashboard.jsx
@@ -61,6 +61,10 @@ function Dashboard({ userData }) {
         .querySelector("meta[name='csrf-token']")
         .getAttribute("content");
 
+    if (Object.entries(event).length == 0) {
+      return;
+    }
+
     const formData = new FormData();
     Object.entries(event).forEach(([key, file]) => {
       formData.append("file_name", file.name);
@@ -97,15 +101,6 @@ function Dashboard({ userData }) {
   return (
     <>
       <ToastContainer />
-      {/* <Label for="exampleCustomFileBrowser">Upload CSV</Label>
-      <Input
-        type="file"
-        id="exampleCustomFileBrowser"
-        name="customFile"
-        accept=".csv"
-        multiple
-        onChange={fileUpload}
-      /> */}
       <Button
         color="success"
         className="csv-button"
