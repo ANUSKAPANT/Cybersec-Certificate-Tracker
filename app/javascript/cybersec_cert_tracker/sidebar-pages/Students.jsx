e9cd6fe3ee64ca11cdde86462ddd60eef30e39a4
Sabina Adhikari <sabinaadhikaris@Sabinas-MacBook-Pro.local>
2022-11-03 14:42:46 -0500

    add students in Students Tab

--- app/javascript/cybersec_cert_tracker/sidebar-pages/Students.jsx
+++ app/javascript/cybersec_cert_tracker/sidebar-pages/Students.jsx
@@ -10,6 +10,11 @@ import "react-toastify/dist/ReactToastify.css";
 import ClipLoader from "react-spinners/ClipLoader";
 import "../Dashboard.css";
 import Jsona from "jsona";
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const override = {
   display: "block",
@@ -35,6 +40,30 @@ function Students({ userData }) {
   const [loading, setLoading] = useState(true);
   const [students, setStudents] = useState([]);
   const [open, setOpen] = React.useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setOpen(false);
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 900,
+    height: 700,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  };
 
   const fetchRecords = async () => {
     axios
@@ -132,11 +161,93 @@ function Students({ userData }) {
       </Button>
       <Modal
         open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
       >
-        <Box sx={style}></Box>
+            <Box
+            component="form"
+            sx={{...style,
+                '& .MuiTextField-root': { m: 1, width: '40ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+           
+            
+            <div>
+            <h2>Add Student Information</h2>
+                <TextField
+                label="Full Name"
+                defaultValue="Full Name"
+                />
+
+                <TextField
+                label="Email Address"
+                defaultValue="Email Address"
+                />
+
+            <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Canvas Course Enrollment</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Canvas Course Enrollment"
+                    >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <TextField
+                label="Company Name"
+                defaultValue="Company Name"
+                />
+
+                <TextField
+                label="Title"
+                defaultValue="Title"
+                />
+                
+
+                <TextField
+                label="Canvas Course Progress"
+                defaultValue="Canvas Course Progress"
+                />
+
+                <TextField
+                label="SMC"
+                defaultValue="SMC"
+                />
+
+               
+
+                <TextField
+                label="Registration Date"
+                defaultValue="Registration Date"
+                />
+
+                <TextField
+                label="DCLDP Code"
+                defaultValue="DCLDP Code"
+                />
+
+                <TextField
+                label="Voucher Purchased Date"
+                defaultValue="Voucher Purchased Date"
+                />
+
+                <TextField
+                label="Voucher Use By"
+                defaultValue="Voucher Use By"
+                />
+
+            <Button onClick={handleClose}>Close</Button>
+            <Button variant="contained" onClick={handleClose}>Submit</Button>
+            </div>
+            </Box>        
       </Modal>
       {loading == true ? (
         <div className="spinner-container">
