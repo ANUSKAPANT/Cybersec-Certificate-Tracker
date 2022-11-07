f3a1972c516da6d02f86f04dd08d5ab2cbbb5e86
anuskapant <anuskapant@gmail.com>
2022-11-06 18:52:21 -0600

    fix for delete courses

--- app/models/cert_voucher.rb
+++ app/models/cert_voucher.rb
@@ -1,5 +1,5 @@
 class CertVoucher < ApplicationRecord
-  belongs_to :student_course
+  belongs_to :student_course, dependent: :destroy
   has_one :exam
   validates :certification_name, presence: true
 end
