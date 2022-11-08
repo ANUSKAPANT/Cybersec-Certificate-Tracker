a27d4a20ca937fbf4267d9d0af5773a0f03fc3f7
anuskapant <anuskapant@gmail.com>
2022-11-07 21:00:38 -0600

    edit and add form added to exams page

--- app/javascript/cybersec_cert_tracker/sidebar-pages/Exams.jsx
+++ app/javascript/cybersec_cert_tracker/sidebar-pages/Exams.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState} from "react";
 import "../table.css";
 import DashboardTable from "../DashboardTable";
-import { Button } from "reactstrap";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
+import { Col, Button, Form, FormGroup, Label, Input, Card, CardBody, Modal,
+  ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ClipLoader from "react-spinners/ClipLoader";
 import "../Dashboard.css";
 import Jsona from "jsona";
+import Select from "react-select";
 
 const override = {
   display: "block",
@@ -29,12 +29,19 @@ const style = {
   p: 4,
 };
 
+const passedOptions = [
+  { label: "Passed", value: true },
+  { label: "Failed", value: false }
+];
+
 const dataFormatter = new Jsona();
 
 function Exams({ userData }) {
   const [loading, setLoading] = useState(true);
   const [exams, setExams] = useState([]);
   const [open, setOpen] = React.useState(false);
+  const [examInfo, setExamInfo] = useState({ id: null });
+  const [certificateVouchers, setCertificateVouchers] = useState([]);
 
   const fetchRecords = async () => {
     axios
@@ -54,13 +61,9 @@ function Exams({ userData }) {
             cert_voucher: "",
           };
         });
-
         setLoading(false);
         setExams(examsData);
-      })
-
-      .catch((error) => {
-        console.log(error);
+      }).catch((error) => {
         toast.error("Error in fetching records", {
           position: "bottom-center",
           autoClose: 3000,
@@ -70,16 +73,70 @@ function Exams({ userData }) {
       });
   };
 
+
+  const fetchCertVouchers = async () => {
+    axios
+      .get(`/cert_vouchers`, {
+        headers: { Authorization: `Bearer ${userData.token}` },
+      })
+      .then((response) => {
+        const data = dataFormatter.deserialize(response.data);
+        const certificateVouchersData = data.map((cert_voucher) => {
+          return {
+            value: cert_voucher.id,
+            label: cert_voucher.certification_name,
+          };
+        });
+        setCertificateVouchers(certificateVouchersData);
+      })
+      .catch((error) => {
+        toast.error("Error in fetching records", {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+      });
+  };
+
+  const fetchExam = async (id) => {
+    axios
+      .get(`/exams/${id}`, {
+        headers: { Authorization: `Bearer ${userData.token}` },
+      })
+      .then((response) => {
+        const exam = dataFormatter.deserialize(response.data);
+        const examsData = {
+          id: exam.id,
+          exam_code: exam.exam_code,
+          certification_name: exam.cert_voucher.certification_name, 
+          exam_date: exam.exam_date,
+          exam_grade: exam.exam_grade,
+          passed: exam.passed,
+          cert_voucher_id: exam.cert_voucher.id,
+        };
+        setExamInfo(examsData);
+      }).catch((error) => {
+        toast.error("Error in fetching records", {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+      });
+  };
+
+
   useEffect(() => {
     fetchRecords();
+    fetchCertVouchers();
   }, []);
 
   const deleteRecords = async (idx) => {
-    axios
-      .delete(`/exams/${idx}`, {
+    axios.delete(`/exams/${idx}`, {
         headers: { Authorization: `Bearer ${userData.token}` },
       })
-      .then((res) => {
+      .then(() => {
         toast.success("Successfully Deleted", {
           position: "bottom-center",
           autoClose: 3000,
@@ -87,7 +144,7 @@ function Exams({ userData }) {
           closeOnClick: true,
         });
       })
-      .catch((err) => {
+      .catch(() => {
         toast.error("Error in deletingrecords", {
           position: "bottom-center",
           autoClose: 3000,
@@ -105,6 +162,72 @@ function Exams({ userData }) {
     });
   };
 
+  const editItem = (id) => {
+    setOpen(true);
+    fetchExam(id);
+  }
+
+  const handleClose = () => {
+    setExamInfo({ id: null });
+    setOpen(false);
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setExamInfo({...examInfo, [name]: value});
+  };
+
+  const handleSelectChange = (value, name) => {
+    setExamInfo({...examInfo, [name]: value.value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let csrf = "";
+    //Not present always
+    if (document.querySelector("meta[name='csrf-token']"))
+      csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+      const {
+        id, cert_voucher_id, exam_code, exam_date, passed, exam_grade
+      } = examInfo;
+  
+      const method = id !== null ? 'patch' : 'post';
+      const url = id == null ? '/exams' : `/exams/${id}`;
+      const message = id !== null ? 'Updated' : 'Created';
+      const data = {
+        cert_voucher_id: cert_voucher_id,
+        exam_code,
+        exam_date,
+        passed,
+        exam_grade,
+      };
+      axios.request({
+        method,
+        url,
+        headers: {
+          "Content-type": "application/json",
+          "X-CSRF-Token": csrf,
+        },
+        data
+      }).then(() => {
+        toast.success(`Successfully ${message}`, {
+          position: "bottom-center",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+        fetchRecords();
+        handleClose();
+      }).catch((error) => {
+        toast.error("Error Occured", {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+    });
+  }
+
   return (
     <>
       <ToastContainer />
@@ -114,15 +237,60 @@ function Exams({ userData }) {
         onClick={() => setOpen(true)}
         id="uploadCSVButton"
       >
-        + Add Exams
+        + Add Exam
       </Button>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}></Box>
+      <Modal isOpen={open} toggle={handleClose} size="lg" style={{maxWidth: '700px', width: '100%'}}>
+        <ModalHeader toggle={handleClose} style={{border: "none"}}>Add Exam</ModalHeader>
+        <ModalBody>
+          <Form>
+            <Card>
+              <CardBody>
+                <FormGroup row>
+                  <Col sm={6}>
+                    <Label for="certification_name" sm={6}>Certification Name</Label>
+                    <Select
+                      name="certification_name"
+                      onChange={(value) => handleSelectChange(value, "cert_voucher_id")}
+                      options={certificateVouchers}
+                      value={certificateVouchers.filter((option) => (examInfo.cert_voucher_id == option.value))}
+                      placeholder="Select Cetification Name"
+                    />
+                  </Col>
+                  <Col sm={6}>
+                    <Label for="exam_code" sm={6}>Exam Code</Label>
+                    <Input name="exam_code" id="exam_code" defaultValue={examInfo.exam_code} onChange={handleInputChange} />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Col sm={6}>
+                    <Label for="exam_date" sm={6}>Exam Date</Label>
+                    <Input name="exam_date" id="exam_date" defaultValue={examInfo.exam_date}  onChange={handleInputChange} />
+                  </Col>
+                  <Col sm={6}>
+                    <Label for="passed" sm={6}>Passed</Label>
+                    <Select
+                      name="passed"
+                      onChange={(value) => handleSelectChange(value, "passed")}
+                      options={passedOptions}
+                      value={passedOptions.filter((option) => (examInfo.passed == option.value))}
+                      placeholder="Select Passed"
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Col sm={6}>
+                    <Label for="exam_grade" sm={6}>Exam Grade</Label>
+                    <Input name="exam_grade" id="exam_grade" defaultValue={examInfo.exam_grade}  onChange={handleInputChange} />
+                  </Col>
+                </FormGroup>
+              </CardBody>
+            </Card>
+          </Form>
+        </ModalBody>
+        <ModalFooter style={{border: "none"}}>
+          <Button color="primary" onClick={handleSubmit}>Submit</Button>{' '}
+          <Button color="secondary" onClick={handleClose}>Cancel</Button>
+        </ModalFooter>
       </Modal>
       {loading == true ? (
         <div className="spinner-container">
@@ -134,7 +302,7 @@ function Exams({ userData }) {
       ) : exams.length === 0 ? (
         <div className="">No Table Records to Show</div>
       ) : (
-        <DashboardTable data={exams} type="Exam" deleteItem={deleteItem} />
+        <DashboardTable data={exams} type="Exam" deleteItem={deleteItem} editItem={editItem} />
       )}
     </>
   );
