3ed98dfc48fe6b0b62ae7ea42c3749fd2f8fac9e
anuskapant <anuskapant@gmail.com>
2022-11-08 23:31:04 -0600

    role based auth added for api

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,9 +2,13 @@ class BaseController < ActionController::Base
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include ActionView::Helpers::SanitizeHelper
 
-  before_action :authenticate
+  before_action :authenticate, :authorize_admin
   helper_method :current_user
   serialization_scope :current_user
+
+  def authorize_admin
+    return render json: { message: 'only accessible to admin' }, status: :unprocessable_entity unless @current_user.admin?
+  end
 
   def current_user
     @current_user
