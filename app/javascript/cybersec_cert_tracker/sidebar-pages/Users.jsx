c5368c0ee36473d7963f21e532dbd7940a541b12
anuskapant <anuskapant@gmail.com>
2022-11-08 22:43:16 -0600

    users edit and add form added

--- app/javascript/cybersec_cert_tracker/sidebar-pages/Users.jsx
+++ app/javascript/cybersec_cert_tracker/sidebar-pages/Users.jsx
@@ -1,34 +1,20 @@
-import React, { useEffect, useState, CSSProperties } from "react";
+import React, { useEffect, useState } from "react";
 import "../table.css";
 import DashboardTable from "../DashboardTable";
-import { Button } from "reactstrap";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
-import { FileUploader } from "react-drag-drop-files";
+import { Col, Button, Form, FormGroup, Label, Input, Card, CardBody, Modal,
+  ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ClipLoader from "react-spinners/ClipLoader";
 import "../Dashboard.css";
 import Jsona from "jsona";
+import Select from "react-select";
 
-const override = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+const role = [
+  { label: "User", value: "user"},
+  { label: "Admin", value: "admin"}
+];
 
 const dataFormatter = new Jsona();
 
@@ -36,6 +22,7 @@ function Users({ userData }) {
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
   const [open, setOpen] = React.useState(false);
+  const [userInfo, setUserInfo] = useState({ id: null });
 
   const fetchRecords = () => {
     axios
@@ -48,13 +35,14 @@ function Users({ userData }) {
           return {
             id: user.id,
             email: user.email,
-            role: user.role
+            role: user.role,
+            first_name: user.first_name,
+            last_name: user.last_name,
           };
         });
         setLoading(false);
         setUsers(userData);
-      }).catch((error) => {
-        console.log(error);
+      }).catch(() => {
         toast.error("Error in fetching records", {
           position: "bottom-center",
           autoClose: 3000,
@@ -69,9 +57,15 @@ function Users({ userData }) {
   }, []);
 
   const deleteRecords = (idx) => {
+    let csrf = "";
+    if (document.querySelector("meta[name='csrf-token']"))
+      csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
     axios
       .delete(`/users/${idx}`, {
-        headers: { Authorization: `Bearer ${userData.token}` },
+        headers: {
+          "Authorization": `Bearer ${userData.token}`,
+          "X-CSRF-Token": csrf,
+        },
       })
       .then((res) => {
         toast.success("Successfully Deleted", {
@@ -99,6 +93,93 @@ function Users({ userData }) {
     });
   };
 
+  const fetchUser = async (id) => {
+    axios
+      .get(`/users/${id}`, {
+        headers: { Authorization: `Bearer ${userData.token}` },
+      })
+      .then((response) => {
+        const user = dataFormatter.deserialize(response.data);
+        const userData = {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          role: user.role
+        };
+        console.log(userData);
+        setUserInfo(userData);
+      }).catch((error) => {
+        console.log(error);
+        toast.error("Error in fetching records", {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+      });
+  };
+
+  const editItem = (id) => {
+    setOpen(true);
+    fetchUser(id);
+  }
+
+  const handleClose = () => {
+    setUserInfo({ id: null });
+    setOpen(false);
+  }
+
+  const handleInputChange = (value, name) => {
+    setUserInfo({...userInfo, [name]: value});
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let csrf = "";
+    //Not present always
+    if (document.querySelector("meta[name='csrf-token']"))
+      csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+      const { id, first_name, last_name, email, password, role } = userInfo;
+  
+      const method = id !== null ? 'patch' : 'post';
+      const url = id == null ? '/users' : `/users/${id}`;
+      const message = id !== null ? 'Updated' : 'Created';
+      const data = { first_name, last_name, email, password, role }
+      axios.request({
+        method,
+        url,
+        headers: {
+          "Content-type": "application/json",
+          "X-CSRF-Token": csrf,
+          "Authorization": `Bearer ${userData.token}`,
+        },
+        data
+      }).then((res) => {
+        console.log(res);
+        toast.success(`Successfully ${message}`, {
+          position: "bottom-center",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+        fetchRecords();
+        handleClose();
+      }).catch((error) => {
+        console.log(error);
+        toast.error("Error Occured", {
+          position: "bottom-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+        });
+    });
+  }
+
+  const handleSelectChange = (value, name) => {
+    setUserInfo({...userInfo, [name]: value.value});
+  }
+
   return (
     <>
       <ToastContainer />
@@ -108,15 +189,52 @@ function Users({ userData }) {
         onClick={() => setOpen(true)}
         id="uploadCSVButton"
       >
-        + Add Users
+        + Add User
       </Button>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}></Box>
+      <Modal isOpen={open} toggle={handleClose} size="lg" style={{maxWidth: '700px', width: '100%'}}>
+        <ModalHeader toggle={handleClose} style={{border: "none"}}>Add User</ModalHeader>
+        <ModalBody>
+          <Form>
+            <Card>
+              <CardBody>
+                <FormGroup row>
+                  <Col sm={6}>
+                    <Label for="first_name" sm={5}>First Name</Label>
+                    <Input name="first_name" id="first_name" defaultValue={userInfo.first_name} onChange={handleInputChange}/>
+                  </Col>
+                  <Col sm={6}>
+                    <Label for="last_name" sm={5}>Last Name</Label>
+                    <Input name="last_name" id="last_name" defaultValue={userInfo.last_name} onChange={handleInputChange}/>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Col sm={6}>
+                    <Label for="email" sm={5}>Email</Label>
+                    <Input name="email" id="email" defaultValue={userInfo.email} onChange={handleInputChange}/>
+                  </Col>
+                  <Col sm={6}>
+                    <Label for="password" sm={5}>Password</Label>
+                    <Input name="password" id="password" defaultValue={userInfo.password} onChange={handleInputChange}/>
+                  </Col>
+                  <Col sm={6}>
+                    <Label for="role" sm={6}>Role</Label>
+                    <Select
+                      name="role"
+                      onChange={(value) => handleSelectChange(value, "role")}
+                      options={role}
+                      value={role.filter((option) => (userInfo.role == option.value))}
+                      placeholder="Select Role"
+                    />
+                  </Col>
+                </FormGroup>
+              </CardBody>
+            </Card>
+          </Form>
+        </ModalBody>
+        <ModalFooter style={{border: "none"}}>
+          <Button color="primary" onClick={handleSubmit}>Submit</Button>{' '}
+          <Button color="secondary" onClick={handleClose}>Cancel</Button>
+        </ModalFooter>
       </Modal>
       {loading == true ? (
         <div className="spinner-container">
@@ -128,7 +246,7 @@ function Users({ userData }) {
       ) : users.length === 0 ? (
         <div className="">No Table Records to Show</div>
       ) : (
-        <DashboardTable data={users} type="User" deleteItem={deleteItem} />
+        <DashboardTable data={users} type="User" deleteItem={deleteItem} editItem={editItem} />
       )}
     </>
   );
