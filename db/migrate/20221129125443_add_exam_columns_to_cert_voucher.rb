632c5d2b17a4f51d0457635218e1ec27330a2c1f
aayush97 <agautam1997@gmail.com>
2022-11-29 07:19:07 -0600

    Fix a bug in cert_voucher migration

--- db/migrate/20221129125443_add_exam_columns_to_cert_voucher.rb
+++ db/migrate/20221129125443_add_exam_columns_to_cert_voucher.rb
@@ -1,8 +1,7 @@
 class AddExamColumnsToCertVoucher < ActiveRecord::Migration[6.0]
   def change
     add_column :cert_vouchers, :exam_code, :string
-    add_column :cert_vouchers, :exam_date, :string
-    add_column :cert_vouchers, :date, :string
+    add_column :cert_vouchers, :exam_date, :date
     add_column :cert_vouchers, :test_result, :string
     add_column :cert_vouchers, :score, :int
     add_column :cert_vouchers, :test_center_id, :string
