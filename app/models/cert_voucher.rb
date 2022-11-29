f605321a47ccc03b407c032b1bee1389e3b2e4a4
aayush97 <agautam1997@gmail.com>
2022-11-29 07:31:58 -0600

    Fix a bug with inclusion validation

--- app/models/cert_voucher.rb
+++ app/models/cert_voucher.rb
@@ -2,6 +2,6 @@ class CertVoucher < ApplicationRecord
   belongs_to :student_course, optional: true
   validates :certification_name, presence: true
   validates :voucher_code, uniqueness: true
-  validates :test_result, { in: %w(pass fail TBD),
+  validates :test_result, inclusion: { in: ["pass", "fail", "TBD"],
     message: "%{value} is not a valid test_result" }
 end
