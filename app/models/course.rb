f7c4e550078f0a829fb3bcb035fa9e9cbb8e1cfe
aayush97 <agautam1997@gmail.com>
2022-11-29 07:04:46 -0600

    Add uniqueness criteria on course name

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ApplicationRecord
   belongs_to :vendor
   has_many :student_courses, dependent: :destroy
   has_many :students, through: :student_courses
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
